b3a6df1831fba2f4a5af37c4493c7c88a4c935bd
Griffin1110 <30826345+Griffin1110@users.noreply.github.com>
2018-10-13 11:00:19 +1100

    This looks pretty good now
    ok please ignore that

--- commands/Moderation/unban.js
+++ commands/Moderation/unban.js
@@ -1,25 +1,27 @@
-const { Command } = require('klasa');
+const {
+   Command
+} = require('klasa');
 
 module.exports = class extends Command {
 
-	constructor(...args) {
-		super(...args, {
-			permissionLevel: 6,
-			requiredPermissions: ['BAN_MEMBERS'],
-			runIn: ['text'],
-			description: 'Unbans a user.',
-			usage: '<user:user> [reason:string] [...]',
-			usageDelim: ' '
-		});
-	}
+   constructor(...args) {
+      super(...args, {
+         permissionLevel: 6,
+         requiredPermissions: ['BAN_MEMBERS'],
+         runIn: ['text'],
+         description: 'Unbans a user.',
+         usage: '<user:user> [reason:string] [...]',
+         usageDelim: ' '
+      });
+   }
 
-	async run(message, [user, ...reason]) {
-		const bans = await message.guild.fetchBans();
-		if (bans.has(user.id)) {
-			await message.guild.members.unban(user, reason.join(' '));
-		} else throw 'This user was not banned so I can not unban them.';
+   async run(message, [user, ...reason]) {
+      const bans = await message.guild.fetchBans();
+      if (bans.has(user.id)) {
+         await message.guild.members.unban(user, reason.join(' '));
+      } else throw 'This user was not banned so I can not unban them.';
 
-		return message.sendMessage(`${user.tag} was unbanned.${reason ? ` With reason of: ${reason}` : ''}`);
-	}
+      return message.sendMessage(`${user.tag} was unbanned.${reason ? ` With reason of: ${reason}` : ''}`);
+   }
 
 };
